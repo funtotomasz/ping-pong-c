6f4e458dbb86fd71599825e5e24e3a0285f36248
Your Name <you@example.com>
2025-07-22 13:44:32 +0200

    feat: Add startup instruction screen

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <ncurses.h>
 #include <unistd.h>
+#include <string.h>
 
 // Ustawienia planszy
 #define WIDTH 80
@@ -33,6 +34,39 @@ void draw_paddle(WINDOW *win, GameObject paddle, int color_pair) {
     wattroff(win, COLOR_PAIR(color_pair));
 }
 
+void show_instructions(WINDOW *win) {
+    werase(win);
+    box(win, 0, 0);
+    
+    char *title = "STEROWANIE";
+    mvwprintw(win, 2, (WIDTH - strlen(title)) / 2, "%s", title);
+
+    mvwprintw(win, 5, 10, "Gracz 1 (lewa paletka):");
+    mvwprintw(win, 7, 12, "W - Gora");
+    mvwprintw(win, 8, 12, "S - Dol");
+    mvwprintw(win, 9, 12, "A - Lewo");
+    mvwprintw(win, 10, 12, "D - Prawo");
+
+    mvwprintw(win, 5, WIDTH - 35, "Gracz 2 (prawa paletka):");
+    mvwprintw(win, 7, WIDTH - 33, "Strzalka w gore - Gora");
+    mvwprintw(win, 8, WIDTH - 33, "Strzalka w dol  - Dol");
+    mvwprintw(win, 9, WIDTH - 33, "Strzalka w lewo - Lewo");
+    mvwprintw(win, 10, WIDTH - 33, "Strzalka w prawo- Prawo");
+
+    char *quit_msg = "Q - Wyjscie z gry";
+    mvwprintw(win, 15, (WIDTH - strlen(quit_msg)) / 2, "%s", quit_msg);
+
+    char *start_msg = "Nacisnij dowolny klawisz, aby rozpoczac...";
+    mvwprintw(win, HEIGHT - 3, (WIDTH - strlen(start_msg)) / 2, "%s", start_msg);
+
+    wrefresh(win);
+
+    // Czekaj na wciśnięcie klawisza (blokująco)
+    nodelay(win, FALSE);
+    wgetch(win);
+    nodelay(win, TRUE); // Wróć do trybu nieblokującego dla gry
+}
+
 int main() {
     // Inicjalizacja ncurses
     initscr();
@@ -48,7 +82,9 @@ int main() {
     // Utworzenie okna gry
     WINDOW *win = newwin(HEIGHT, WIDTH, (LINES - HEIGHT) / 2, (COLS - WIDTH) / 2);
     keypad(win, TRUE);
-    nodelay(win, TRUE); // Nie blokuj pętli, czekając na klawisz
+
+    // Pokaż instrukcje i czekaj na start
+    show_instructions(win);
 
     // Pozycje startowe i granice
     const int player1_start_x = 2;
